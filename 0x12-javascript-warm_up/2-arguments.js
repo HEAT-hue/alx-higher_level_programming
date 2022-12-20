dd6ab73f06f4f45bcef4d2d193575a56b1fa984d
mightGuy <onyejemeemmanuel65@gmail.com>
2022-12-20 02:37:20 +0100

    Fixed command line arguments output

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,12 @@
 #!/usr/bin/node
 const process = require('process');
 if (process.argv[2]) {
-  console.log('Argument found');
+	if (process.argv[3]) {
+		console.log('Arguments found');
+	}
+	else {
+  		console.log('Argument found');
+	}
 } else {
   console.log('No argument');
 }
